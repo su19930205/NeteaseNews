ca707dd2bce891d0ee5109163f4c5555ae52d2cc
szh <469041032@qq.com>
2015-08-29 14:14:32 +0800

    初始化项目

---
+++ 新闻/新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新闻
+//
+//  Created by mac on 15/8/29.
+//  Copyright (c) 2015年 szh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新闻/新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新闻
+//
+//  Created by mac on 15/8/29.
+//  Copyright (c) 2015年 szh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
