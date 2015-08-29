16fbe81f9efaefa6cca891daeb82da10003e5a43
szh <469041032@qq.com>
2015-08-29 14:27:58 +0800

    搭建项目框架

--- 新闻/新闻/Classes/News/NewsTableViewController.h
+++ 新闻/新闻/Classes/News/NewsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NewsTableViewController.h
 //  新闻
 //
 //  Created by mac on 15/8/29.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface NewsTableViewController : UITableViewController
 
 @end
-
